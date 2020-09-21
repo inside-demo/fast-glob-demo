3d6edf931d339c454a9ed36d867861508454f21e
Ivan Demidov <Scrum@list.ru>
2020-09-21 15:33:54 +0300

    add meow

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const meow = require('meow');
 const fg = require('fast-glob');
 
-const entries = fg.sync('test/fixtures/index.html')
+const cli = meow();
+
+console.log({input: cli.input})
+
+const entries = fg.sync(cli.input)
 
 console.log({entries})
